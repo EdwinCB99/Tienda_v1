d986be4c82e88bdf3585217cdc8c528869fca8da
EdwinCB99 <109781851+EdwinCB99@users.noreply.github.com>
2024-07-14 18:24:13 -0600

    Semana 7
    Semana 7 firebase

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -7,4 +7,13 @@ import java.util.List;
 public interface CategoriaService {
     //Se obtiene un listado de categorias en un List
     public List<Categoria> getCategorias(boolean activos);
+    
+    //se obtiene una categoria por su id
+    public Categoria getCategoria(Categoria categoria);
+    
+    //metodo de guardar
+    public void save(Categoria categoria);
+    
+    //metodo de eliminar
+    public void delete(Categoria categoria);
 }
